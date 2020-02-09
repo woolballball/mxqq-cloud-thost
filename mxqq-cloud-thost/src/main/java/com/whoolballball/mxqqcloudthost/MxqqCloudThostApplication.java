eed9542fc0fc2beec37afaa80241aa49a00ff80d
whoolballball <inetinfo@126.com>
2020-02-09 19:41:00 +0800

    v0.0.1-snapshot

---
+++ mxqq-cloud-thost/src/main/java/com/whoolballball/mxqqcloudthost/MxqqCloudThostApplication.java
@@ -0,0 +1,13 @@
+package com.whoolballball.mxqqcloudthost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MxqqCloudThostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MxqqCloudThostApplication.class, args);
+    }
+
+}
---
+++ mxqq-cloud-thost/src/test/java/com/whoolballball/mxqqcloudthost/MxqqCloudThostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whoolballball.mxqqcloudthost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MxqqCloudThostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
